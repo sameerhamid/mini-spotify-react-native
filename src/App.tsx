49a881bd0b854aa6023e1dfe1e24ed6215f6a7df
a <a>
2024-02-19 16:23:21 +0530

    add track player and created servies for music

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+
+      <Text>Testing</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
