5836baca8df717acaf490019288d2de77713ea80
a <a>
2024-02-19 16:47:09 +0530

    creted useEffect and call the method add track

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {
+  ActivityIndicator,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -8,7 +9,31 @@ import {
   Text,
 } from 'react-native';
 
+import {setupPlayer, addTrack} from '../musicPlayerServices';
+
 function App(): React.JSX.Element {
+  const [isPlayerReady, setIsplayerReady] = useState(false);
+
+  async function setup() {
+    const isSetup = await setupPlayer();
+    if (isSetup) {
+      await addTrack();
+    }
+
+    setIsplayerReady(isSetup);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator size={'large'} />
+      </SafeAreaView>
+    );
+  }
   return (
     <SafeAreaView>
       <StatusBar />
@@ -18,6 +43,10 @@ function App(): React.JSX.Element {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
